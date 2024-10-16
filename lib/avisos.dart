69c6f2d2057247782861d2c7e0829890b97e901b
Ana Carolina <carolinaanaa191@gmail.com>
2024-10-16 09:37:49 -0300

    atualizacao avisos

--- lib/avisos.dart
+++ lib/avisos.dart
@@ -19,12 +19,40 @@ class _AvisosState extends State<Avisos> {
      ),
      body: Container(
       padding: EdgeInsets.all(32),
-      child: Column(
+      child:Column(
+         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Text("")
-        ],
+          Row(
+            children: [
+              
+              Icon(Icons.warning, color: Colors.yellow),
+                  SizedBox(width: 8),
+                  Text('Em virtude da situação emergencial das queimadas na região, dia 26/08 as aulas serão de forma remota. Para os alunos que não puderem assistir as aulas, deverão entregar atividades para computação de frequência. ', style: TextStyle(fontSize: 18)),
+                ],
+              ),
+               SizedBox(height: 16), // Espaço entre as linhas
+              Row(
+                children: [
+                  Icon(Icons.warning, color: Colors.yellow),
+                  SizedBox(width: 8),
+                  Text('Dia 10/10, quinta-feira, aulão com a professora Josi no salão nobre às 08:30.', style: TextStyle(fontSize: 18)),
+                ],
+              ),
+               SizedBox(height: 16), // Espaço entre as linhas
+              Row(
+                children: [
+                  Icon(Icons.warning, color: Colors.yellow),
+                  SizedBox(width: 8),
+                  Text('Devido á problemas na rede de água da unidade, amanhã 11/10, sexta-feira, as aulas serão remotas pelo teams e o trote será adiado, sem data prevista !', style: TextStyle(fontSize: 18)),
+                ],
+              ),
+
+            ],
+          )
+        
          ),
-     ),
-    );
+     );
+     
+    
   }
 }
